ddc02ead3d20d186a6906b251c40d8b612f1682d
Vladimir <no@email>
2019-08-20 01:29:13 +0300

    truncate, strip

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,19 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(limit: Int = 16) : String{
+    var s = this.trim()
+    if (s.length < limit) return s
+    else {
+        s = this.substring(0, limit)
+        if (s.last() == ' ') s = s.substring(0, s.lastIndex)
+        s += "..."
+        return s
+    }
+}
+
+fun String.stripHtml() : String {
+    var s = this
+    s = s.replace(Regex("<[^>]+>"), "")
+    s = s.replace(Regex(" {2,}"), " ")
+    return s
+}
