ac19790ab47efa395c956ccc74f1455780a4c418
Vladimir Korkin <v.korkin@noone.ru>
2019-08-20 09:48:01 +0300

    builder fix

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -32,14 +32,14 @@ data class User(
     }
 
     data class Builder(
-    val id: String = "",
+    var id: String = "",
     var firstName: String? = null,
     var lastName: String? = null,
     var avatar: String? = null,
     var rating: Int = 0,
     var respect: Int = 0,
-    val lastVisit: Date? = Date(),
-    val isOnline: Boolean = false
+    var lastVisit: Date? = Date(),
+    var isOnline: Boolean = false
     )
     {
     	fun id(s: String) = apply {this.id = s}
@@ -48,7 +48,7 @@ data class User(
     	fun avatar(s: String) = apply {this.avatar = s}
     	fun rating(n: Int) = apply {this.rating = n}
     	fun respect(n: Int) = apply {this.respect = n}
-    	fun lastVisit(d: Date) = apply {this.lastVisit = s}
+    	fun lastVisit(d: Date) = apply {this.lastVisit = d}
     	fun isOnline(b: Boolean) = apply {this.isOnline = b}
     	fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
     }
